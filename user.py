064d69d6e948ea0319a87a040dc927d89f3c3bd1
M0nicah <monicawughanga@gmail.com>
2022-04-23 11:00:18 +0300

    add save new user method and tests it

--- user.py
+++ user.py
@@ -1,8 +1,12 @@
 class User:
-
-    user_details = []     # empty user details
+    user_details = []  # empty user details
 
     def __init__(self, username, user_password):
         self.username = username
         self.user_password = user_password
 
+    def save_user(self):
+        """
+        save_contact method saves user objects into contact_list
+        """
+        User.user_details.append(self)
--- user_test.py
+++ user_test.py
@@ -1,6 +1,7 @@
 import unittest
 from user import User
 
+
 class TestUser(unittest.TestCase):
     def setUp(self):
         self.new_user = User("Monica", "@monica")
@@ -9,10 +10,17 @@ class TestUser(unittest.TestCase):
         """
         test_init test case to test if the object is initialized properly
         """
-
         self.assertEqual(self.new_user.username, "Monica")  # add assertion here
         self.assertEqual(self.new_user.user_password, "@monica")
 
+    def test_save_user(self):
+       """
+        test_save_contact test case to test if the user object is saved into the user_details list
+       """
+       self.new_user.save_user()  # saving the new contact
+       self.assertEqual(len(User.user_details), 1)
 
 if __name__ == '__main__':
     unittest.main()
+
+
