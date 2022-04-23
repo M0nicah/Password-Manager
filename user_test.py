79809bd76dc0b09d3899bf2775abcfb1a6038921
M0nicah <monicawughanga@gmail.com>
2022-04-23 14:01:48 +0300

    add delete user method and tested it

--- user_test.py
+++ user_test.py
@@ -14,13 +14,12 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.user_password, "@monica")
 
     def test_save_user(self):
-       """
+        """
         test_save_contact test case to test if the user object is saved into the user_details list
        """
-       self.new_user.save_user()  # saving the new contact
-       self.assertEqual(len(User.user_details), 1)
+        self.new_user.save_user()  # saving the new contact
+        self.assertEqual(len(User.user_details), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
